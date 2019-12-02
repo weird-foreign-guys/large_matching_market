d46dcf538376158ec538ba059d9a88a7bce2c78a
ankile <lars.ankile@gmail.com>
2019-12-02 17:52:40 -0500

    Make basic version of preference generation

--- prefgen.py
+++ prefgen.py
@@ -3,8 +3,7 @@ import math
 import numpy as np
 
 
-def exp_prob(n):
-    delta = 2
+def exp_prob(n, delta=2):
     start = -delta
     end = delta
 
@@ -13,57 +12,82 @@ def exp_prob(n):
     nums = np.exp(-xs)
     probabilities = nums / nums.sum()
 
-    # prob_dict = {i: prob for i, prob in enumerate(probabilities)}
+    prob_dict = {i: prob for i, prob in enumerate(probabilities)}
 
-    return probabilities  # prob_dict
+    return prob_dict
 
 
 def lin_prob(n):
     """
     Very simple linear probability distribution
     """
-    sum_ = n*(n+1) / 2
+    sum_ = n * (n + 1) / 2
     return [(n - i) / sum_ for i in range(n)]
 
 
-def generate1(n, k, corr):
+def generate1(n, k, p):
     res = []
     choices = [i for i in range(n)]
-    p = exp_prob(n)
+    p = list(p.values())
 
-    for i in range(n):
+    for _ in range(n):
         res.append(choice(choices, replace=False, p=p, size=k))
 
     return res
 
 
-def generate2(n, preferences):
+def generate2(n, k, preferences, p):
 
     # List to hold the choices the proposee has to choose
     # between when generating preferences
     all_choices = []
 
+    # Check for choices
     for recipient in range(n):
         choices = set()
         for proposer, prefs in enumerate(preferences):
-            if recipient in set(prefs):
+            if recipient in prefs:
                 choices.add(proposer)
         all_choices.append(choices)
 
-    return all_choices
+    # Generate the preferences of the other side
+    # based on the same probability distribution and the choices
+    new_preferences = []
+
+    for recipient, choices in enumerate(all_choices):
+
+        prob = np.array([p[choice] for choice in choices])
+
+        prob = prob / prob.sum()
+
+        if len(choices) > 0:
+            new_preferences.append(
+                choice(list(choices), replace=False,
+                       p=prob, size=len(choices))
+            )
+        else:
+            new_preferences.append([])
+
+    return new_preferences
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    print(exp_prob(10))
+    n = 100
+    k = 5
+    delta = 2
+    p = exp_prob(n, delta)
 
-    # result = generate1(10, 7, 1)
+    print("First side:")
 
-    # for pref in result:
+    result = generate1(n, k, p)
 
-    #     print(pref)
+    for pref in result:
+        print(pref)
 
-    # result2 = generate2(10, result)
+    print("\nSecond side:")
 
-    # for choices in result2:
-    #     print(choices)
+    result2 = generate2(n, k, result, p)
+
+    for choices in result2:
+        print(choices)
