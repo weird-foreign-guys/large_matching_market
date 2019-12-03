7ffbf3fc3ce50e5f37c9d20ed8e892caa6076271
Yuto Yagi <35690865+yutoyagi@users.noreply.github.com>
2019-12-02 20:07:47 -0500

    debugged

--- useful_deviation.py
+++ useful_deviation.py
@@ -37,7 +37,7 @@ def count_useful_deviatiors(male_prefs, female_prefs):
 """
 
 def count_useful_deviatiors(male_prefs, female_prefs):
-    truthful_match = deferred_acceptance(male_prefs, female_prefs)
+    _, truthful_match = deferred_acceptance(male_prefs, female_prefs)
     dev_female_prefs = deepcopy(female_prefs)
     count = 0
     #Run |females| "person has devaition" test
@@ -45,10 +45,12 @@ def count_useful_deviatiors(male_prefs, female_prefs):
         #Run O(|males|) devaiations for female, continue while devaitions are possbile
         while dev_female_prefs[female] != []:
             dev_female_prefs[female] = dev_female_prefs[female][:-1]
-            matching_d = deferred_acceptance(male_prefs, dev_female_prefs)
+            _, matching_d = deferred_acceptance(male_prefs, dev_female_prefs)
+
             if(useful_deviation(truthful_match, matching_d, female, female_prefs)):
                 count += 1
                 break
+        dev_female_prefs[female] = female_prefs[female]
     return count
 
 
@@ -56,14 +58,14 @@ def count_useful_deviatiors(male_prefs, female_prefs):
 # Run after defered acceptance to check if a useful matching has occured. If True, can continue to next deviation.
 def useful_deviation(truthful_match, deviated_match, deviator, proposed_prefs):
     preference_order = proposed_prefs[deviator]
-
+    """
     print(deviator)
-    print(deviated_match[deviator])
     print(deviated_match)
-    print(truthful_match[deviator])
+    print(deviated_match[deviator])
     print(truthful_match)
+    print(truthful_match[deviator])
     print(preference_order)
-
+    """
     # TODO deal with "NA"
     if deviated_match[deviator] == "NA":
         # if deviation gives no match, it is never useful
