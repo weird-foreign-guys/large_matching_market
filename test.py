573eda76c78c636bfcb4709ccc6e53f303097aeb
Yuto Yagi <35690865+yutoyagi@users.noreply.github.com>
2019-12-04 15:55:13 -0500

    refactoring DA

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from useful_deviation import count_useful_deviatiors
 from prefgen import get_preferences
+import time
 
 write_to_file = True
 
@@ -10,9 +11,12 @@ write_to_file = True
 if __name__ == "__main__":
     k = 10
     output = {}
-    for n in range(10, 101, 50):
+    for n in range(100, 201, 50):
         male_prefs, female_prefs = get_preferences(n, k)
+        start_time = time.time()
         useful_deviators_cnt = count_useful_deviatiors(male_prefs, female_prefs)
+        elapsed_time = time.time() - start_time
+        print(elapsed_time)
         ratio = useful_deviators_cnt / float(n)
         output[n] = ratio
         print(f"result n={n}: ", ratio)
