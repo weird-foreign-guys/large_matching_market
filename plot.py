0f82c1067c5a5132d93a7dde70f2c91c5f61129e
ankile <lars.ankile@gmail.com>
2019-12-04 20:28:35 -0500

    Enhance plotting and add data

--- plot.py
+++ plot.py
@@ -19,27 +19,37 @@ def plot(ks=[10, 15, 20], ds=[0.05, 1.0, 3.0]):
     for col, d in enumerate(ds, start=1):
         plt.subplot(1, len(ds), col)
         for k in ks:
+            # Find all relevant files and put them in a list
             files = glob.glob(f"d={d};*k={k};*")
 
+            # Initialize the array to hold the raw data from the files
             data = []
-
             for file in files:
                 with open(file) as f:
                     data.extend(map(lambda x: x[:-1].split(";"), f.readlines()))
-
+            
+            # Do some initial cleaning of the data before it is put into a dictionary
             data = list(map(lambda x: (int(x[0]), float(x[1].replace(",", "."))), data))
 
+            # Initialize a dictionary to collect all results from different files
             agg_data = defaultdict(lambda: {"sum": 0, "n": 0})
-
             for n, percentage in data:
                 agg_data[n]["sum"] += percentage
                 agg_data[n]["n"] += 1
 
+            # Aggregate the data based on the data in the dictionary
             dataseries = [(key, val["sum"] / val["n"]) for key, val in agg_data.items()]
 
             dataseries = sorted(dataseries, key=itemgetter(0))
 
-            plt.plot(*zip(*dataseries))
+            plt.plot(*zip(*dataseries), label=f"k={k}")
+    
+        plt.legend()
+        plt.title(f"ρ={d}")
+
+        plt.xlabel("n")
+        plt.ylabel("D(n)/n")
+
     plt.show()
 
 
